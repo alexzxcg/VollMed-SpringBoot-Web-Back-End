324ebc7f5774518cdd3dcfe63f3f0a490a10827f
Alex <alex-xp123@hotmail.com>
2024-10-15 16:01:58 -0300

    Implementada ordenacao na paginacao

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.model.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listarMedicosCadastrados(Pageable paginacao) {
+    public Page<DadosListagemMedico> listarMedicosCadastrados(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
